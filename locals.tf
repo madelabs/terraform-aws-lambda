28042153ad58e61bc978af2bb828c3ae12b3488a
Holden Porter <77367306+ml-holdenporter@users.noreply.github.com>
2023-09-29 16:46:24 -0400

    add python support

--- locals.tf
+++ locals.tf
@@ -1,11 +1,13 @@
 locals {
   dotnet6_placeholder  = "placeholder-csharp.zip"
   nodejs18_placeholder = "placeholder-js.zip"
+  python3_placeholder  = "placeholder-python3.zip"
 
   placeholder = lookup(
     {
-      "dotnet6"  = local.dotnet6_placeholder
-      "nodejs18" = local.nodejs18_placeholder
+      "dotnet6"    = local.dotnet6_placeholder
+      "nodejs18.x" = local.nodejs18_placeholder
+      "python3.x"  = local.python3_placeholder
     }, var.function_runtime, null
   )
 }
