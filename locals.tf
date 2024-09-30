70b1865ff4cd90fb3da8cd35083016d7228fcd95
Christopher Boyd <christopher.boyd@kehe.com>
2024-09-30 17:34:04 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -11,6 +11,7 @@ locals {
         "dotnet6"    = local.dotnet6_placeholder,
         "dotnet8"    = local.dotnet8_placeholder,
         "nodejs18.x" = local.nodejs18_placeholder,
+        "python3.10" = local.nodejs18_placeholder,
         "python3.x"  = local.python3_placeholder
     }, var.function_runtime, null)
     : null
